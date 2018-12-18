a5f69841ec1e5d9034e355309dcb9657ebb0e702
dydwo1127@gmail.com <dydwo1127@gmail.com>
2018-12-19 03:17:07 +0900

    코드정리

--- PrinterControl/PrinterControl.ino
+++ PrinterControl/PrinterControl.ino
@@ -20,12 +20,6 @@ void setup() {
   stepper2.setSpeed(100);
   stepper1.release();
   stepper2.release();
-  /*if(Serial.available()){
-    while(Serial.available()){
-      Serial.read();
-    }
-  }
-  delay(50);*/
 }
 
 void loop() {
@@ -53,49 +47,3 @@ void loop() {
     digitalWrite(2, LOW);
   }
 }
-
-/*void wordToBraille(String words){
-  int wordLength = words.length();
-  int codeArray[wordLength][6];
-  for(int i = 0; i<wordLength; i++){
-    wordToCode(codeArray[i],words[i]);
-  }
-  for(int i = 0; i<wordLength; i++){
-    for(int j = 0; j<6; j++){
-      Serial.print(codeArray[i][j]);
-    }
-  }
-  Serial.println();
-  for(int i = 0; i<wordLength; i++){
-    Serial.print(codeArray[i][0]);
-    Serial.print(codeArray[i][1]);
-  }
-  Serial.println();
-  for(int i = 0; i<wordLength; i++){
-    Serial.print(codeArray[i][2]);
-    Serial.print(codeArray[i][3]);
-  }
-  Serial.println();
-  for(int i = 0; i<wordLength; i++){
-    Serial.print(codeArray[i][4]);
-    Serial.print(codeArray[i][5]);
-  }
-  Serial.println();
-}*/
-
-/*void Print(String textLine){
-  isPrinting = true;
-  if(textLine == "#paperOut"){
-    Serial.println("Paper Out");
-    isPrinting = false;
-    return;
-  }
-  Serial.println(textLine);
-  wordToBraille(textLine);
-  for(int i =0; i<10;i++){
-    Serial.print(".");
-    delay(100);
-  }
-  Serial.println("done");
-  isPrinting = false;
-}*/

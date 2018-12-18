bf18c34c57826fe356af8bde8c6b460239047c9b
dydwo1127@gmail.com <dydwo1127@gmail.com>
2018-12-19 03:14:47 +0900

    대문자, 숫자 추가

--- PrinterControl/wordToCode.ino
+++ PrinterControl/wordToCode.ino
@@ -208,6 +208,214 @@ void wordToCode(int code[], char singleWord){
       code[4] = 1;
       code[5] = 1;
       break;
+    case 'A':
+      code[0] = 1;
+      code[1] = 0;
+      code[2] = 0;
+      code[3] = 0;
+      code[4] = 0;
+      code[5] = 0;
+      break;
+    case 'B':
+      code[0] = 1;
+      code[1] = 0;
+      code[2] = 1;
+      code[3] = 0;
+      code[4] = 0;
+      code[5] = 0;
+      break;
+    case 'C':
+      code[0] = 1;
+      code[1] = 1;
+      code[2] = 0;
+      code[3] = 0;
+      code[4] = 0;
+      code[5] = 0;
+      break;
+    case 'D':
+      code[0] = 1;
+      code[1] = 1;
+      code[2] = 0;
+      code[3] = 1;
+      code[4] = 0;
+      code[5] = 0;
+      break;
+    case 'E':
+      code[0] = 1;
+      code[1] = 0;
+      code[2] = 0;
+      code[3] = 1;
+      code[4] = 0;
+      code[5] = 0;
+      break;
+    case 'F':
+      code[0] = 1;
+      code[1] = 1;
+      code[2] = 1;
+      code[3] = 0;
+      code[4] = 0;
+      code[5] = 0;
+      break;
+    case 'G':
+      code[0] = 1;
+      code[1] = 1;
+      code[2] = 1;
+      code[3] = 1;
+      code[4] = 0;
+      code[5] = 0;
+      break;
+    case 'H':
+      code[0] = 1;
+      code[1] = 0;
+      code[2] = 1;
+      code[3] = 1;
+      code[4] = 0;
+      code[5] = 0;
+      break;
+    case 'I':
+      code[0] = 0;
+      code[1] = 1;
+      code[2] = 1;
+      code[3] = 0;
+      code[4] = 0;
+      code[5] = 0;
+      break;
+    case 'J':
+      code[0] = 0;
+      code[1] = 1;
+      code[2] = 1;
+      code[3] = 1;
+      code[4] = 0;
+      code[5] = 0;
+      break;
+    case 'K':
+      code[0] = 1;
+      code[1] = 0;
+      code[2] = 0;
+      code[3] = 0;
+      code[4] = 1;
+      code[5] = 0;
+      break;
+    case 'L':
+      code[0] = 1;
+      code[1] = 0;
+      code[2] = 1;
+      code[3] = 0;
+      code[4] = 1;
+      code[5] = 0;
+      break;
+    case 'M':
+      code[0] = 1;
+      code[1] = 1;
+      code[2] = 0;
+      code[3] = 0;
+      code[4] = 1;
+      code[5] = 0;
+      break;
+    case 'N':
+      code[0] = 1;
+      code[1] = 1;
+      code[2] = 0;
+      code[3] = 1;
+      code[4] = 1;
+      code[5] = 0;
+      break;
+    case 'O':
+      code[0] = 1;
+      code[1] = 0;
+      code[2] = 0;
+      code[3] = 1;
+      code[4] = 1;
+      code[5] = 0;
+      break;
+    case 'P':
+      code[0] = 1;
+      code[1] = 1;
+      code[2] = 1;
+      code[3] = 0;
+      code[4] = 1;
+      code[5] = 0;
+      break;
+    case 'Q':
+      code[0] = 1;
+      code[1] = 1;
+      code[2] = 1;
+      code[3] = 1;
+      code[4] = 1;
+      code[5] = 0;
+      break;
+    case 'R':
+      code[0] = 1;
+      code[1] = 0;
+      code[2] = 1;
+      code[3] = 1;
+      code[4] = 1;
+      code[5] = 0;
+      break;
+    case 'S':
+      code[0] = 0;
+      code[1] = 1;
+      code[2] = 1;
+      code[3] = 0;
+      code[4] = 1;
+      code[5] = 0;
+      break;
+    case 'T':
+      code[0] = 0;
+      code[1] = 1;
+      code[2] = 1;
+      code[3] = 1;
+      code[4] = 1;
+      code[5] = 0;
+      break;
+    case 'U':
+      code[0] = 1;
+      code[1] = 0;
+      code[2] = 0;
+      code[3] = 0;
+      code[4] = 1;
+      code[5] = 1;
+      break;
+    case 'V':
+      code[0] = 1;
+      code[1] = 0;
+      code[2] = 1;
+      code[3] = 0;
+      code[4] = 1;
+      code[5] = 1;
+      break;
+    case 'W':
+      code[0] = 0;
+      code[1] = 1;
+      code[2] = 1;
+      code[3] = 1;
+      code[4] = 0;
+      code[5] = 1;
+      break;
+    case 'X':
+      code[0] = 1;
+      code[1] = 1;
+      code[2] = 0;
+      code[3] = 0;
+      code[4] = 1;
+      code[5] = 1;
+      break;
+    case 'Y':
+      code[0] = 1;
+      code[1] = 1;
+      code[2] = 0;
+      code[3] = 1;
+      code[4] = 1;
+      code[5] = 1;
+      break;
+    case 'Z':
+      code[0] = 1;
+      code[1] = 0;
+      code[2] = 0;
+      code[3] = 1;
+      code[4] = 1;
+      code[5] = 1;
+      break;
     case ' ':
       code[0] = 9;
       code[1] = 9;
@@ -215,6 +423,86 @@ void wordToCode(int code[], char singleWord){
       code[3] = 9;
       code[4] = 9;
       code[5] = 9;
+      break;
+    case '1':
+      code[0] = 1;
+      code[1] = 0;
+      code[2] = 0;
+      code[3] = 0;
+      code[4] = 0;
+      code[5] = 0;
+      break;
+    case '2':
+      code[0] = 1;
+      code[1] = 0;
+      code[2] = 1;
+      code[3] = 0;
+      code[4] = 0;
+      code[5] = 0;
+      break;
+    case '3':
+      code[0] = 1;
+      code[1] = 1;
+      code[2] = 0;
+      code[3] = 0;
+      code[4] = 0;
+      code[5] = 0;
+      break;
+    case '4':
+      code[0] = 1;
+      code[1] = 1;
+      code[2] = 0;
+      code[3] = 1;
+      code[4] = 0;
+      code[5] = 0;
+      break;
+    case '5':
+      code[0] = 1;
+      code[1] = 0;
+      code[2] = 0;
+      code[3] = 1;
+      code[4] = 0;
+      code[5] = 0;
+      break;
+    case '6':
+      code[0] = 1;
+      code[1] = 1;
+      code[2] = 1;
+      code[3] = 0;
+      code[4] = 0;
+      code[5] = 0;
+      break;
+    case '7':
+      code[0] = 1;
+      code[1] = 1;
+      code[2] = 1;
+      code[3] = 1;
+      code[4] = 0;
+      code[5] = 0;
+      break;
+    case '8':
+      code[0] = 1;
+      code[1] = 0;
+      code[2] = 1;
+      code[3] = 1;
+      code[4] = 0;
+      code[5] = 0;
+      break;
+    case '9':
+      code[0] = 0;
+      code[1] = 1;
+      code[2] = 1;
+      code[3] = 0;
+      code[4] = 0;
+      code[5] = 0;
+      break;
+    case '0':
+      code[0] = 0;
+      code[1] = 1;
+      code[2] = 1;
+      code[3] = 1;
+      code[4] = 0;
+      code[5] = 0;
       break;
     default:
       break;
